7a65722b0cd23bbff6f027f4a6b1596e437ddab1
syedabdulsamad <thesamad@gmail.com>
2015-04-06 16:31:07 +0300

    ---- Calculator Initial version

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,12 +12,72 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
+    var userIsTying = false
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if(userIsTying){
+            enter()
+        }
+        switch (operation){
+            case "×":
+                performOperation {$0 * $1}
+            case "−":
+                performOperation {$1 - $0}
+            case "+":
+                performOperation {$0 + $1}
+            case "÷":
+                performOperation {$1 / $0}
+            case "√":
+                performOperation() {sqrt($0)}
+        default: break
+        }
+    }
+    
+    
     @IBAction func appendDigit(sender: UIButton) {
         let digit =  sender.currentTitle!
         println("digit = \(digit)")
+        if(!userIsTying){
+            userIsTying = true
+            display.text = sender.currentTitle!
+        } else{
+            display.text = display.text! + sender.currentTitle!
+        }
+        
         
     }
     
+    var displayValue : Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue;
+        }
+        set{
+            userIsTying = false
+            display.text = "\(newValue)"
+        }
+    }
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userIsTying = false
+        operandStack.append(displayValue)
+        println("operandstack : \(operandStack)" )
+    }
+    
+    func performOperation(operation: (Double, Double) ->Double){
+        if (operandStack.count >= 2){
+            displayValue = operation( operandStack.removeLast() , operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double ->Double){
+        if (operandStack.count >= 1){
+            displayValue = operation( operandStack.removeLast())
+            enter()
+        }
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
